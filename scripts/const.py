ca40d148e137fbc963ad357b4cc2d9cb80482642
amylase <amylase@amylasenoMacBook.local>
2018-07-21 05:11:48 +0900

    initial db definition

--- scripts/const.py
+++ scripts/const.py
@@ -10,6 +10,13 @@ with (conf_dir / "id.json").open() as f:
 public_id = _id["public"]
 private_id = _id["private"]
 
+env_file = conf_dir / "env.txt"
+if env_file.exists():
+    with env_file.open() as f:
+        env = f.read().strip()
+else:
+    env = "dev"
+
 
 if __name__ == '__main__':
     print(private_id)
