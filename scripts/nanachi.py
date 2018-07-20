4a864b81411d2f1247c70e7b25930ee935897df1
amylase <amylase@amylasenoMacBook.local>
2018-07-21 06:16:52 +0900

    nanachi initial version

---
+++ scripts/nanachi.py
@@ -0,0 +1,47 @@
+# メイドインアビスをみて
+
+from flask import Flask, render_template, make_response
+from db import get_connection
+import const
+import logging
+
+
+app = Flask(__name__)
+connection = get_connection()
+
+
+@app.route("/models/<name>/blob")
+def model_blob(name: str):
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute("SELECT body FROM tblmodel WHERE name=%s", (name,))
+    blob = cursor.fetchone()[b"body"]
+    cursor.close()
+    response = make_response(blob)
+    response.headers.set('Content-Type', 'application/octet-stream')
+    response.headers.set('Content-Disposition', 'attachment', filename='%s_tgt.mdl' % name)
+    return response
+
+
+@app.route("/models/<name>")
+def model_summary(name: str):
+    return render_template('model_summary.html', name=name)
+
+
+@app.route("/model_list")
+def model_list():
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute("SELECT name FROM tblmodel")
+    rows = cursor.fetchall()
+    cursor.close()
+    return render_template("model_list.html", models=rows)
+
+
+@app.route("/")
+def hello():
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    message_level = logging.WARN if const.env == "prod" else logging.INFO
+    logging.basicConfig(level=message_level)
+    app.run(host="localhost", port=11451)
