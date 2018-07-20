c460b03f763b66ad6174c2051ac735b3f4eedbcc
Osamu Koga <osak.63@gmail.com>
2018-07-20 15:09:27 -0500

    ちっちゃくて見づらい

--- saverwing/convert.sh
+++ saverwing/convert.sh
@@ -1,7 +1,7 @@
 for f in raw/*.png; do
     target="thumbnails/$(basename $f)"
-    if [ ! -f ${target} ]; then
+    #if [ ! -f ${target} ]; then
         echo "Convert $f"
-        convert -resize 400 $f thumbnails/$(basename $f)
-    fi
+        convert -trim -resize 300 -gravity center -extent 400x400 -background black $f thumbnails/$(basename $f)
+    #fi
 done
