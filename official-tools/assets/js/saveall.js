544b43023fc46eec9f6fe6042349fcc0d0587e4b
Osamu Koga <osak.63@gmail.com>
2018-07-21 15:17:08 -0500

    手元で確認するのめんどいからサーバに上げよ

--- official-tools/assets/js/saveall.js
+++ official-tools/assets/js/saveall.js
@@ -1,3 +1,12 @@
+function waitBeforeRun(f, wait) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            f();
+            resolve();
+        }, wait);
+    });
+}
+
 async function saveAll(event) {
     for(const f of event.target.files) {
         if (!f) {
@@ -5,16 +14,26 @@ async function saveAll(event) {
             continue;
         }
         const reader = new FileReader();
+
         await new Promise((resolve) => {
             reader.onload = async function (e) {
                 modelBData = new Uint8Array(e.target.result);
                 document.getElementById('viewModel').click();
-                setTimeout(() => {
+                await waitBeforeRun(() => {
+                    vis.render();
+                    vis.postImage('http://localhost:25252', `${f.name}.png`);
+                }, 2000);
+                await waitBeforeRun(() => {
+                    vis.setRotation(Math.PI / 4, Math.PI / 4);
+                    vis.render();
+                    vis.postImage('http://localhost:25252', `${f.name}-up-45deg.png`);
+                }, 2000);
+                await waitBeforeRun(() => {
                     vis.setRotation(Math.PI / 4, -Math.PI / 4);
                     vis.render();
                     vis.postImage('http://localhost:25252', `${f.name}-up-315deg.png`);
-                    resolve();
-                }, 5000);
+                }, 2000);
+                resolve();
             };
             reader.readAsArrayBuffer(f);
         });
