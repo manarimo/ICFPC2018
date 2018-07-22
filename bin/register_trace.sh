321a135101d75a1d75ab245ba23158fb0668d361
Osamu Koga <osak.63@gmail.com>
2018-07-22 08:10:03 -0500

    ha??????

--- bin/register_trace.sh
+++ bin/register_trace.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 # Usage: register_trace.sh <model name e.g. LA001> <nbt file> <energy>
-/user/ubuntu/venv/bin/python /user/ubuntu/ICFPC2018/scripts/register_trace.py $@
+/home/ubuntu/venv/bin/python /user/ubuntu/ICFPC2018/scripts/register_trace.py $@
