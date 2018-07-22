17892cc80756f7b6c988b6a8d710c45a67a05c8e
Osamu Koga <osak.63@gmail.com>
2018-07-22 02:30:22 -0500

    Update autoscorer_fill to follow DB change

--- scripts/autoscorer_fill.rb
+++ scripts/autoscorer_fill.rb
@@ -7,10 +7,28 @@ traces = JSON.parse(`curl http://nanachi.kadingel.osak.jp/api/pending_traces`)
 traces['traces'].each do |trace|
   puts "trace_id #{trace['trace_id']}: #{trace['model_name']} by #{trace['author']}"
   nbt_file = "/tmp/autoscorer-#{Process.pid}-#{trace['trace_id']}.nbt"
-  mdl_file = "/tmp/autoscorer-#{Process.pid}-#{trace['model_name']}.mdl"
   `curl -o #{nbt_file} http://nanachi.kadingel.osak.jp/traces/#{trace['trace_id']}/blob`
-  `curl -o #{mdl_file} http://nanachi.kadingel.osak.jp/models/#{trace['model_name']}/blob`
-  score = `../autoscorer/autoscorer #{mdl_file} #{nbt_file}`
+
+  if trace['src_model']
+    src_mdl_file = "/tmp/autoscorer-#{Process.pid}-#{trace['src_model']}.mdl"
+    `curl -o #{src_mdl_file} http://nanachi.kadingel.osak.jp/models/#{trace['src_model']}/blob`
+  end
+  if trace['tgt_model']
+    tgt_mdl_file = "/tmp/autoscorer-#{Process.pid}-#{trace['tgt_model']}.mdl"
+    `curl -o #{tgt_mdl_file} http://nanachi.kadingel.osak.jp/models/#{trace['tgt_model']}/blob`
+  end
+
+  if src_mdl_file && tgt_mdl_file
+    puts "This is reassembly problem"
+    score = `../autoscorer/autoscorer --reassembly #{src_mdl_file} #{tgt_mdl_file} #{nbt_file}`
+  elsif src_mdl_file
+    puts "This is disassembly problem"
+    score = `../autoscorer/autoscorer --disassembly #{src_mdl_file} #{nbt_file}`
+  else
+    puts "This is assembly problem"
+    score = `../autoscorer/autoscorer #{tgt_mdl_file} #{nbt_file}`
+  end
+
   if $?.success?
     puts "#{trace['model_name']}: #{score}"
     puts `curl -X POST -F energy=#{score.to_i} http://nanachi.kadingel.osak.jp/traces/#{trace['trace_id']}/update-autoscorer`
