8dbc6d772465f7d67ec2c6223486459c125157a7
Osamu Koga <osak.63@gmail.com>
2018-07-23 03:30:43 -0500

    dummy_fd

--- bin/run_dummy_fd.sh
+++ bin/run_dummy_fd.sh
@@ -9,10 +9,10 @@
 # mkut
 #URL="http://kadingel.osak.jp/job/mkut%E3%81%AEAI%E3%81%B6%E3%82%93%E5%9B%9E%E3%81%99%E3%81%9E/buildWithParameters"
 
-URL="http://kadingel.osak.jp/job/%E8%AA%B0%E3%81%8B%E3%81%AEAI%E3%81%B6%E3%82%93%E5%9B%9E%E3%81%99%E3%81%9E"
+URL="http://kadingel.osak.jp/job/%E8%AA%B0%E3%81%8B%E3%81%AEAI%E3%81%B6%E3%82%93%E5%9B%9E%E3%81%99%E3%81%9E/buildWithParameters"
 
 #for i in {001..186}; do
-for i in {187..199}
+for i in {187..199}; do
     echo $i
     curl "${URL}?PROBLEM_ID=FD${i}&AUTHOR=kawatea&COMMENT=shiva&token=a&RUN_SCRIPT=ai/kawatea/run_shiva.sh"
 done
